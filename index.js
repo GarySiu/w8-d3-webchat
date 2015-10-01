db1e33cf1d4c11833469a5b21713307248a4c6d1
Gary Siu <Gary@untypeable.com>
2015-10-01 07:56:59 +0100

    Removed unneeded dependancies

--- index.js
+++ index.js
@@ -1,15 +1,10 @@
 var express = require('express')
 var app = express()
 var port = process.env.PORT || 3000
-var bodyParser = require('body-parser')
 var server = require('http').Server(app)
 var io = require('socket.io')(server)
 
 app.use(express.static(__dirname + '/public'))
-
-// app.get('/', function(req,res){
-//   res.send('Hello World!')
-// })
 
 io.on('connection', function(socket){
   console.log('A user connected')
